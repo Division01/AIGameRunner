e65f89b5945529e71048bde936852652414405d9
Division01 <vincent.fischer@hotmail.com>
2019-05-10 18:30:22 +0200

    Ajoute des suites de 3 positives

--- ai/IA V1.1.py
+++ ai/IA V1.1.py
@@ -29,8 +29,11 @@ class Server:
         move = self.coupRandom(body,you)
         print(move["cube"], move["direction"])
 
+        message = "Je joue les X"
+        if you == 1 :
+            message = "Je joue les O"
 
-        return {"move": move}
+        return {"move": move, "message" : message}
 
     def coupRandom(self, body,player):
 
@@ -95,25 +98,41 @@ class Server:
         index_free = 0
 
         for i in range(len(body["game"])):
-            if body["game"][i] == you:
-                if i % 5 == 0:
+            if body["game"][i] == you:                                                      #On cherche une case à nous
+                if i % 5 == 0:                                                              #Dans la première colonne
                     count = 0
-                    for j in range(5):
+                    for j in range(5):                                                      #On a une case dans la première colonne, on voit si d'autres suivent en ligne
                         if body["game"][i+j] == you:
                             count += 1
                         else :
                             index_free = (i+j)
+                    if count == 3:
+                        side = "gauche"
+                        return self.play_for_pre_win(index_free, "ligne",him,body,you, side)
                     if count == 4:
                         return self.play_for_win(index_free, "ligne",him,body,you)
                     
-                elif (i-1) % 5 == 0:
+                elif (i-1) % 5 == 0:                                                        #Dans la deuxième colonne
                     index_free = i-1
                     count = 0
                     for j in range(4):
                         if body["game"][i+j] == you:
                             count += 1
+                    if count == 3 and (i == 1 or i == 21) :
+                        side = "milieu"
+                        return self.play_for_pre_win(index_free, "ligne",him,body,you, side)
                     if count == 4:
                         return self.play_for_win(index_free, "ligne",him,body,you)
+
+                elif (i-2) % 5 == 0:                                                        #Dans la troisième colonne pour trois qui se suiveraient
+                    index_free = i-2
+                    count = 0
+                    for j in range(3):
+                        if body["game"][i+j] == you:
+                            count += 1
+                    if count == 3:
+                        side = "droit"
+                        return self.play_for_pre_win(index_free, "ligne",him,body,you, side)
                     
                 else:
                     self.check_col(body,you,him)
@@ -121,7 +140,7 @@ class Server:
                 if i % 5 == 0:
                     count = 0
                     for j in range(5):
-                        if body["game"][i+j] == you:
+                        if body["game"][i+j] == him:
                             count += 1
                         else :
                             index_free = (i+j)
@@ -132,7 +151,7 @@ class Server:
                     index_free = i-1
                     count = 0
                     for j in range(4):
-                        if body["game"][i+j] == you:
+                        if body["game"][i+j] == him:
                             count += 1
                     if count == 4:
                         return self.play_for_counter(index_free, "ligne",him,body,you)
@@ -149,23 +168,39 @@ class Server:
                 if i in range(5):
                     count = 0
                     for j in range(5):
-                        if body["game"][i+5+j] == you:
+                        if body["game"][i+5*j] == you:
                             count += 1
                         else:
-                            index_free = (i+5+j)
+                            index_free = (i+5*j)
+                    if count == 3:
+                        side = "haut"
+                        return self.play_for_pre_win(index_free, "colonne",him,body,you, side)                      #index de la cinquieme ligne                
                     if count == 4:
                         return self.play_for_win(index_free,"colonne",him,body,you)
                 elif (i-5) in range(5):
                     count = 0
                     for j in range(4):
-                        if body["game"][i+5+j] == you:
+                        if body["game"][i+5*j] == you:
                             count += 1
                         else:
-                            index_free = (i+5+j)
+                            index_free = (i+5*j)
+                    if count == 3 and (i == 5 or i == 9):
+                        side = "milieu"
+                        return self.play_for_pre_win(index_free, "colonne",him,body,you, side)                  #index de la cinquieme ligne
                     if count == 4:
                         return self.play_for_win(index_free,"colonne",him,body,you)
+                elif (i-10) in range(5):
+                    count = 0
+                    for j in range(3) : 
+                        if body["game"][i+5*j] == you:
+                            count += 1
+                    if count == 3 :
+                        side = "bas"
+                        index_free = i - 10
+                        return self.play_for_pre_win(index_free, "colonne",him,body,you, side)
                 else :
                     self.coupRandom(body,you)
+
             elif body["game"][i] == him:
                 if i in range(5):
                     count = 0
@@ -187,6 +222,54 @@ class Server:
                         return self.play_for_counter(index_free,"colonne",him,body,you)
                 else :
                     self.coupRandom(body,you)
+
+
+    def play_for_pre_win(self,index,direction,him,body,you, side):
+
+        print('PLAYFORPREWIN')
+
+        if direction == "ligne":
+            if body["game"][index] != him and side == "gauche" :                    #Cas où trois à gauche sont bons, l'index est celui de la 5eme colonne
+                return {"cube" : index, "direction" : "W"}                    #NoteToSelf, tu peux ajouter le cas où on est en première ou dernière ligne et profiter de l'autre extremite du coup
+            elif body["game"][index] != him and side == "droit" :                       #Cas où trois à droite sont bons, l'index est celui de la première colonne
+                return {"cube" : index, "direction" : "E"}
+            elif side == "milieu" and index == 0 :                                      
+                for i in range(1,5) :
+                    if body["game"][5*i] != him :
+                        return {"cube" : 5*i, "direction" : "N"}
+                    elif body["game"][4 + 5*i] != him :
+                        return {"cube" : 4 + 5*i, "direction" : "N"}
+            elif side == "milieu" and index == 20 :
+                for i in range(4) :    
+                    if body["game"][5*i] != him :
+                        return {"cube" : 5*i, "direction" : "S"}
+                    elif body["game"][4 + 5*i] != him :
+                        return {"cube" : 4 + 5*i, "direction" : "S"}
+
+            else :
+                return self.coupRandom(body,you)
+
+        if direction == "colonne":                      #haut index ligne 5, milieu index ligne 5, bas index ligne 1
+            if body["game"][index] != him and side == "haut" :
+                return {"cube" : index, "direction" : "N"}
+            elif body["game"][index] != him and side == "bas" :
+                return {"cube" : index, "direction" : "S"}
+            elif side == "milieu" and index == 5 :
+                for i in range(1,5) :
+                    if body["game"][i] != him :
+                        return {"cube" : i, "direction" : "W"}
+                    elif body["game"][20 + i] != him :
+                        return {"cube" : 20 + i, "direction" : "W"}
+            elif side == "milieu" and index == 9 :
+                for i in range(1,5) :
+                    if body["game"][4-i] != him :
+                        return {"cube" : 4-i, "direction" : "E"}
+                    elif body["game"][24 - i] != him :
+                        return {"cube" : 24 - i, "direction" : "E"}
+                
+            else:
+                return self.coupRandom(body, you)
+
 
     def play_for_win(self,index,direction,him,body,you):
 
